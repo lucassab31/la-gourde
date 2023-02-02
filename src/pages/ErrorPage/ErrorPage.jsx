997017b6319fd35a28c6535f9c0f594e3e6e3d9e
lucas <luqui31@gmail.com>
2023-02-02 15:51:57 +0100

    bootstrap icons

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -5,8 +5,8 @@ import { ArrowRight } from 'react-bootstrap-icons';
 const ErrorPage = () => {
     return (
         <>
-            <img src={img404} className="img-fluid my-4" alt="404 image" />
             <main className="container">
+                <img src={img404} className="img-fluid my-4" alt="404 image" />
                 <div className="card">
                     <div className="card-body">
                         <h1 className="card-title text-center text-primary">Vous avez dû vous égarer</h1>
