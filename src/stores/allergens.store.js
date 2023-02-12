ed2f6ac4bc6d29d70866bd6cb00b9ac70dc95f55
Lucas Laplanche <36164636+LucasLpc@users.noreply.github.com>
2023-02-12 14:47:52 +0100

    Stores fixed

--- src/stores/allergens.store.js
+++ src/stores/allergens.store.js
@@ -1,36 +1,26 @@
-import {types, flow} from "mobx-state-tree"
-import { getAllergens } from "services/allergens.product"
+import { types, flow } from "mobx-state-tree";
+import { getAllergens } from "services/allergens.product";
 
 export const Allergens = types.model("Allergens", {
   id: types.number,
   name: types.string,
   icon: types.string,
-})
-.actions(self => ({
-  loadAllergens: flow(function* () {
-    try{
-      const allergens = yield getAllergens();
-      self.allergens = allergens;
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }),
-}));
+});
 
-export const AllergensStore = types.model("AllergensStore", {
-  allergens: types.array(Allergens),
-}).actions(self => ({
-  loadAllergens: flow(function* () {
-    try{
-      const allergens = yield getAllergens();
-      self.allergens = allergens;
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }),
-}));
+export const AllergensStore = types
+  .model("AllergensStore", {
+    allergens: types.array(Allergens),
+  })
+  .actions((self) => ({
+    loadAllergens: flow(function* () {
+      try {
+        const allergens = yield getAllergens();
+        self.allergens = allergens;
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+  }));
 
 export const allergensStore = AllergensStore.create({
   allergens: [],
