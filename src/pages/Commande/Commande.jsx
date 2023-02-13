9b1484566bab5ba1aa6e9d1ef0cd4abb41dcfabf
lucas <luqui31@gmail.com>
2023-02-13 23:53:43 +0100

    page paiement

--- src/pages/Commande/Commande.jsx
+++ src/pages/Commande/Commande.jsx
@@ -6,7 +6,19 @@ const { observer } = require("mobx-react-lite");
 
 const Commande = observer(() => {
     const {cart} = useStore();
-    const {products, totalPrice } = cart
+    const {products, totalPrice } = cart;
+
+    const changeCardNumber = (e) => {
+        if (e.target.value.length === 4 || e.target.value.length === 9 || e.target.value.length === 14) {
+            e.target.value += " ";
+        }
+    }
+
+    const changeCardDate = (e) => {
+        if (e.target.value.length === 2) {
+            e.target.value += "/";
+        }
+    }
 
   return (
     <div>
@@ -17,6 +29,30 @@ const Commande = observer(() => {
                 <img src={creditCard} className="img-fluid" alt="Image de carte de crédit" />
             </div>
             <div className="col-md-7">
+                <form action="">
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label">Nom</label>
+                        <input type="text" className="form-control" id="name" placeholder="Jean Dupont" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="cardNumber" className="form-label">Numéro de carte</label>
+                        <input type="text" onKeyDown={changeCardNumber} maxLength={19} className="form-control" id="cardNumber" placeholder="0000 0000 0000 0000" />
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="cardDate" className="form-label">Date d'expiration</label>
+                                <input type="text" pattern="[0-9]{2}/[0-9]{2}" onKeyDown={changeCardDate} maxLength={5} className="form-control" id="cardDate" placeholder="MM/YY" />
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="cardCVC" className="form-label">CVC/CVV</label>
+                                <input type="password" maxLength={3} className="form-control" id="cardCVC" placeholder="000" />
+                            </div>
+                        </div>
+                    </div>
+                </form>
                 <ul className="list-group">
                     {products.map((product) => (
                     <li key={product.id} className="list-group-item">
@@ -27,7 +63,7 @@ const Commande = observer(() => {
                             <div className="ms-3 w-100">
                                 <div className="d-flex align-items-center justify-content-between">
                                     <h4>{product.title}</h4>
-                                    <p className="m-0">{product.size[0].price}€ /unité</p>
+                                    <p className="m-0">{product.quantity} x {product.size[0].price}€</p>
                                 </div>
                                 <div className="d-flex justify-content-between">
                                     <p>Taille : {product.size[0].name} </p>
@@ -41,6 +77,7 @@ const Commande = observer(() => {
                     <h3>Total</h3>
                     <p className="fw-bold fs-2 text-primary">{totalPrice}€</p>
                 </div>
+                <button className="btn btn-primary w-100 mt-2 text-white fw-bold btn-lg">Payer</button>
             </div>
         </div>
     </div>
