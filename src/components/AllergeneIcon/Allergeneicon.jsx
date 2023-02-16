dcfb9b202146497806793ce23403be7964c8c315
Valentin Muller <valmuller14@gmail.com>
2023-02-16 16:46:40 +0100

    tooltip allergens

--- src/components/AllergeneIcon/Allergeneicon.jsx
+++ src/components/AllergeneIcon/Allergeneicon.jsx
@@ -1,15 +1,25 @@
 import style from "./AllergeneIcon.module.scss";
+import { Tooltip, OverlayTrigger } from "react-bootstrap";
 
 const AllergeneIcon = (props) => {
   const { name, title } = props;
   const url = require(`../../images/icon/allergene/${name}`);
+  
+  const tooltip = (
+    <Tooltip id="button-tooltip">
+     {title}
+    </Tooltip>
+  );
+
   return (
     <>
-      <li className="me-3">
-        <div className={style.allergene}>
-          <img src={url} alt={title} />
-        </div>
-      </li>
+      <OverlayTrigger placement="top" overlay={tooltip}>
+          <li className="me-3">
+            <div className={style.allergene}>
+              <img src={url} alt={title} />
+            </div>
+          </li>
+      </OverlayTrigger>
     </>
   );
 };
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="py-3 my-4 border-top border-primary">
       <ul className="nav d-flex justify-content-center border-bottom pb-3 mb-3 gap20">
-        <Nav strClass="nav-item" />
+        <Nav strClass="nav-item d-flex align-items-center" style={{gap:'10px'}} />
       </ul>
       <p className="text-center text-muted">&copy; 2023 La Gourde</p>
     </footer>
