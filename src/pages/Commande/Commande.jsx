d23132700a84358e68e9d79d6395de69d9361b83
Lucas Laplanche <36164636+LucasLpc@users.noreply.github.com>
2023-02-16 11:48:04 +0100

    Pattern date

--- src/pages/Commande/Commande.jsx
+++ src/pages/Commande/Commande.jsx
@@ -68,7 +68,7 @@ const Commande = observer(() => {
                   </label>
                   <input
                     type="text"
-                    pattern="[0-9]{2}/[0-9]{2}"
+                    pattern="(?:0[1-9]|1[0-2])/[0-9]{2}"
                     onKeyDown={changeCardDate}
                     maxLength={5}
                     className="form-control"
@@ -129,95 +129,6 @@ const Commande = observer(() => {
             {/* <Link to="/recap" id="btnPayer" className="btn btn-primary w-100 mt-2 text-white fw-bold btn-lg" disabled>Payer</Link> */}
           </form>
         </div>
-      </div>
-      <div className="col-md-7">
-        <form action="">
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">
-              Nom
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              placeholder="Jean Dupont"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="cardNumber" className="form-label">
-              Numéro de carte
-            </label>
-            <input
-              type="text"
-              onKeyDown={changeCardNumber}
-              maxLength={19}
-              className="form-control"
-              id="cardNumber"
-              placeholder="0000 0000 0000 0000"
-            />
-          </div>
-          <div className="row">
-            <div className="col-md-6">
-              <div className="mb-3">
-                <label htmlFor="cardDate" className="form-label">
-                  Date d'expiration
-                </label>
-                <input
-                  type="text"
-                  pattern="[0-9]{2}/[0-9]{2}"
-                  onKeyDown={changeCardDate}
-                  maxLength={5}
-                  className="form-control"
-                  id="cardDate"
-                  placeholder="MM/YY"
-                />
-              </div>
-            </div>
-            <div className="col-md-6">
-              <div className="mb-3">
-                <label htmlFor="cardCVC" className="form-label">
-                  CVC/CVV
-                </label>
-                <input
-                  type="password"
-                  maxLength={3}
-                  className="form-control"
-                  id="cardCVC"
-                  placeholder="000"
-                />
-              </div>
-            </div>
-          </div>
-        </form>
-        <ul className="list-group">
-          {products.map((product) => (
-            <li key={product.id} className="list-group-item">
-              <div className="d-flex align-items-center ">
-                <div style={{ width: "40px" }}>
-                  <SmoothieIcon color={product.color} />
-                </div>
-                <div className="ms-3 w-100">
-                  <div className="d-flex align-items-center justify-content-between">
-                    <h4>{product.title}</h4>
-                    <p className="m-0">
-                      {product.quantity} x {product.size.price}€
-                    </p>
-                  </div>
-                  <div className="d-flex justify-content-between">
-                    <p>Taille : {product.size.name} </p>
-                  </div>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <div className="d-flex justify-content-between align-items-center mt-2">
-          <h3>Total</h3>
-          <p className="fw-bold fs-2 text-primary">{totalPrice}€</p>
-        </div>
-        <button className="btn btn-primary w-100 mt-2 text-white fw-bold btn-lg">
-          Payer
-        </button>
       </div>
     </div>
   );
